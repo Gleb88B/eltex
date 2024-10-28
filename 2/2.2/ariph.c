e7d5be3db5ba245b1d6e3a516203913671a665d2
Gleb88B <biryukov.gl@gmail.com>
2024-10-29 04:58:05 +0700

    Практика 1 Модуль 2

---
+++ 2/2.2/ariph.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "ariph.h"
+
+float sum(int n, ...)
+{
+    float result = 0;
+    va_list fact;
+    va_start(fact, n);
+    for (int i = 0; i < n; i++)
+    {
+        result += va_arg(fact, double);
+
+    }
+    va_end(fact);
+    return result;
+}
+
+float diff(int n, ...)
+{
+    float result = 0;
+    va_list fact;
+    va_start(fact, n);
+    result = va_arg(fact, double);
+    for (int i =0; i < n - 1; i++)
+    {
+        result -= va_arg(fact, double);
+    }
+    va_end(fact);
+    return result;
+}
+
+float mult(int n, ...)
+{
+    float result = 0;
+    va_list fact;
+    va_start(fact, n);
+    result = va_arg(fact, double);
+    for (int i = 0; i < n - 1; i++)
+    {
+        result *= va_arg(fact, double);
+    }
+    va_end(fact);
+    return result;
+}
+
+float divv(int n, ...)
+{
+    float result = 0;
+    va_list fact;
+    va_start(fact, n);
+    result = va_arg(fact, double);
+    for (int i = 0; i < n - 1; i++)
+    {
+        result /= va_arg(fact, double);
+    }
+    va_end(fact);
+    return result;
+}
