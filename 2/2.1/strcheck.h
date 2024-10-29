58412abed1013985dcce0a3dc42441c3234c4fa0
Gleb88B <biryukov.gl@gmail.com>
2024-10-29 15:02:43 +0700

    Удаление

--- 2/2.1/strcheck.h
+++
@@ -1,7 +0,0 @@
-#ifndef STRCHECK_H_
-#define STRCHECK_H_
-
-void delete_n(char *str);
-int isempty(char *str);
-
-#endif /* STRCHECK_H_ */
--- 2/2.2/ariph.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ARIPH_H
-#define _ARIPH_H
-
-float sum(int n, ...);
-float diff(int n, ...);
-float mult(int n, ...);
-float divv(int n, ...);
-
-#endif
